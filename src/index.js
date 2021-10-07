ccd503510389f400e39f2c4f218292ed912309d1
Yurii Kuliasov <mr.awesome@i.ua>
2021-10-07 16:24:36 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-import './sass/main.scss';
+import menu from './menu.json';
+import refs from './js/refs.js';
+import markUp from './templetes/markUp.handlebars';
+const { menuJS, themeSwitchToggle } = refs;
+let result = markUp(menu);
+menuJS.insertAdjacentHTML('beforeend', result);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+themeSwitchToggle.addEventListener('change', e => {
+  document.body.classList.toggle(Theme.DARK);
+  document.body.classList.toggle(Theme.LIGHT);
+  let theme = e.target.checked ? Theme.DARK : Theme.LIGHT;
+  localStorage.setItem('theme', theme);
+});
+
+let theme = localStorage.getItem('theme');
+
+if (!theme) {
+  theme = Theme.LIGHT;
+  localStorage.setItem('theme', theme);
+}
+document.body.classList.add(theme);
+themeSwitchToggle.checked = theme === Theme.LIGHT ? false : true;
---
+++ src/js/refs.js
@@ -0,0 +1,4 @@
+export default {
+    menuJS: document.querySelector('.js-menu'),
+    themeSwitchToggle: document.getElementById('theme-switch-toggle'),
+  };
